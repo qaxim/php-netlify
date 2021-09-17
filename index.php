abc4c4799407b56cb6112a196dedee1dd275a510
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2021-09-18 01:50:40 +0500

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,27 @@
   $page_name = "Dashboard";
   include "partials/_header.php";
   include "partials/_menu.php";
-  phpinfo();
+
+  $curl = curl_init();
+
+  curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://api.kharidar.co/api/v1/categories',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'GET',
+  ));
+
+  $response = curl_exec($curl);
+
+  curl_close($curl);
+  echo $response;
+
+
+
 ?>
 
 <main role="main" class="container-fluid">
